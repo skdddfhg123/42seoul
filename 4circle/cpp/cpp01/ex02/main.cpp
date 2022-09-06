161e2f241619cfebf21c40957b78357845a6f56c
skdddfhg123 <skdddfhg6@gmail.com>
2022-09-06 13:21:21 +0900

    cpp01 ex02

---
+++ 4circle/cpp/cpp01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idongmin <idongmin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 02:42:20 by idongmin          #+#    #+#             */
+/*   Updated: 2022/09/06 11:44:32 by idongmin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstring>
+#include <iostream>
+
+int main(void)
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
+}
