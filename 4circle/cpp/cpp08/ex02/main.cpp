57110108352116ae46ff62bef00eedacaa7405d3
Dongmin Lee <dongmlee@c2r8s3.42seoul.kr>
2022-09-17 09:06:20 +0900

    cpp08 ex02

---
+++ 4circle/cpp/cpp08/ex02/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongmlee <dongmlee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/17 09:01:41 by dongmlee          #+#    #+#             */
+/*   Updated: 2022/09/17 09:05:43 by dongmlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
+int main()
+{
+    std::cout << std::string(80, '-') << std::endl;
+    std::cout << "Subject Test (MutantStack)" << std::endl;
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	std::cout << std::string(80, '-') << std::endl;
+    std::cout << "myTest (List)" << std::endl;
+	{
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		std::cout << mstack.back() << std::endl;
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(mstack);
+	}
+	return 0;
+}
