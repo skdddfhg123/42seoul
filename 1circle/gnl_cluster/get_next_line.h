85e4e26a759a2fdd8365177812a3a465740ea401
Dongmin Lee <dongmlee@c7r2s10.42seoul.kr>
2022-11-25 18:10:47 +0900

    1circle_cluster

---
+++ 1circle/gnl_cluster/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongmlee <dongmlee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/14 19:47:06 by dongmlee          #+#    #+#             */
+/*   Updated: 2021/09/29 10:32:01 by dongmlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char	*s);
+size_t	ft_strlcpy(char	*dest, const char	*src, size_t	dstsize);
+
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char	*s, int	c);
+char	*ft_strjoin(char	*s1, char const	*s2);
+
+#endif
